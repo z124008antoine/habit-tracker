8b1e4aa26704f9408a04da1f332cc65bf3cc2331
Pierre <167388289+z524026-Pierre@users.noreply.github.com>
2024-07-10 12:48:25 +0900

    New profile pages

---
+++ public/profile.php
@@ -0,0 +1,12 @@
+<?php include __DIR__ . '/auth/guard.php'; ?>
+
+<?php function renderPage() { ?>
+
+
+<?php
+}
+
+$includeHead = "<link rel='stylesheet' href='/styles/profile.css'>";
+$documentTitle = 'Profile';
+include 'layout/layout.php';
+?>
