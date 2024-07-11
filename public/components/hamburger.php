ae521e713bd9fcd3b0fca446d5258dcc160af39f
Antoine Blumenroeder <z124008@shibaura-it.ac.jp>
2024-07-11 18:40:51 +0900

    very cool hamburger menu

--- public/components/hamburger.php
+++ public/components/hamburger.php
@@ -1,33 +1,24 @@
+<?php
+// size: square number, direction: 1, -1
+function line($size, $direction) {
+    for ($i = 0; $i < $size; $i++) {
+        ?>
+        <div class="square" style="transform: translateY(<?= $i * $direction * 100; ?>%); transition-delay: <?= $i * 50 ?>ms;"></div>
+        <?php
+    }
+}
+?>
+
 <button class="hamburger open">
-    <div class="close open"></div>
-    <div class="open"></div>
-    <div class="open"></div>
-    <div class="open"></div>
-    <div class="close open"></div>
-
-    <div class=""></div>
-    <div class="close"></div>
-    <div class=""></div>
-    <div class="close"></div>
-    <div class=""></div>
-
-    <div class="open"></div>
-    <div class="open"></div>
-    <div class="close open"></div>
-    <div class="open"></div>
-    <div class="open"></div>
-
-    <div class=""></div>
-    <div class="close"></div>
-    <div class=""></div>
-    <div class="close"></div>
-    <div class=""></div>
-
-    <div class="close open"></div>
-    <div class="open"></div>
-    <div class="open"></div>
-    <div class="open"></div>
-    <div class="close open"></div>
+    <div class="line">
+        <?= line(7, 1); ?>
+    </div>
+    <div class="line middle">
+        <?= line(7, 0); ?>
+    </div>
+    <div class="line">
+        <?= line(7, -1); ?>
+    </div>
 </button>
 
 <script defer>
