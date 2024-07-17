92f8c810848055cf80cf6f35d4caa0e040fbf373
Z124087 <z124087@shibaura-it.ac.jp>
2024-07-17 15:50:33 +0900

    update profile search + show searched profile

--- public/profiles.php
+++ public/profiles.php
@@ -1,23 +1,30 @@
 <?php include __DIR__ . '/auth/guard.php'; ?>
 
 <?php function renderPage() { ?>
-    
+
 <h1>Search people</h1>
-<input type="text" class="text-input" placeholder="Search">
+<form method="GET">
+    <input type="text" class="text-input" name="search" placeholder="Search">
+    <button class="neon-button" type="submit">Search</button>
+</form>
 
 <div class="results">
     <?php
     include __DIR__ . '/database/users.php';
-    $users = search_users('');
-    foreach ($users as $user) {
-        ?>
-        <div class="result">
-            <a href="/profile.php?user_id=<?= $user['id'] ?>">
-                <img class="profile-pic" src="/images/avatars/avatar_<?= $user['profile_picture'] ?>.png" alt="<?= $user['username'] ?>">
-                <?= $user['username'] ?>
-            </a>
-        </div>
+    $search = isset($_GET['search']) ? $_GET['search'] : '';
+    if (!empty($search)) {
+        $users = search_users($search);
+        foreach ($users as $user) {
+            ?>
+    <div class="result">
+        <a href="/profile_user_search.php?user_id=<?= $user['id'] ?>">
+            <img class="profile-pic" src="/images/avatars/avatar_<?= $user['profile_picture'] ?>.png"
+                alt="<?= $user['username'] ?>">
+            <?= $user['username'] ?>
+        </a>
+    </div>
     <?php
+        }
     }
     ?>
 </div>
@@ -28,4 +35,3 @@
 $documentTitle = 'Profiles'; // Set the title of the document for the layout
 $includeHead = '<link rel="stylesheet" href="/styles/profiles.css">'; // Include the CSS for the page
 include 'layout/layout.php';
-?>
