8ec270aa56ac9201dd4c389809188e806ee25496
Pierre <167388289+z524026-Pierre@users.noreply.github.com>
2024-07-22 18:20:04 +0900

    Avatar customizer page

---
+++ public/avatar_customizer.php
@@ -0,0 +1,16 @@
+<?php include __DIR__ . '/auth/guard.php'; ?>
+
+<?php function renderPage() { ?>
+
+<section>
+    
+</section>
+
+
+<?php
+}
+
+$includeHead = "<link rel='stylesheet' href='/styles/avatar_customizer.css'>";
+$documentTitle = 'Profile';
+include 'layout/layout.php';
+?>
